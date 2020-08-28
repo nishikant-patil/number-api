ed06ecdb46a3feac12c2911b24108dd350b304e8
Nishikant Ajay Patil <crusnik02.trinityblood@gmail.com>
2020-08-28 19:50:37 +0530

    Added unit tests

---
+++ src/test/java/foo/bar/numberapi/PrimeNumberApiTests.java
@@ -0,0 +1,27 @@
+package foo.bar.numberapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+public final class PrimeNumberApiTests {
+
+    private final PrimeNumberApi primeNumberApi = new PrimeNumberApi();
+    
+    @Test
+    public void testIsPrimeWithPrime() {
+        Assert.isTrue(primeNumberApi.isPrime(23));
+        Assert.isTrue(primeNumberApi.isPrime(37));
+    }
+
+    @Test
+    public void testIsPrimeWithComposite() {
+        Assert.isTrue(!primeNumberApi.isPrime(22));
+    }
+
+    @Test
+    public void testIsPrimeWithInvalid() {
+        Assert.isTrue(!primeNumberApi.isPrime(0));        
+    }
+}
